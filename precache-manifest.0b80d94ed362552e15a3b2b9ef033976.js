79a7efe22cdb1d9812bfbe2a2cdaa1ee750ab222
Kune-San <oniel808@gmail.com>
2020-07-09 14:17:41 +0800

    Updates

---
+++ precache-manifest.0b80d94ed362552e15a3b2b9ef033976.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd90ee8c9a683c6cadb40ce5262f61ee",
+    "url": "/portfolio2/index.html"
+  },
+  {
+    "revision": "c7b1d60adb6b2b85ad45",
+    "url": "/portfolio2/static/css/main.b8145381.chunk.css"
+  },
+  {
+    "revision": "c5e8b3fd1739388fc987",
+    "url": "/portfolio2/static/js/2.268b74ba.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio2/static/js/2.268b74ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7b1d60adb6b2b85ad45",
+    "url": "/portfolio2/static/js/main.67350fbe.chunk.js"
+  },
+  {
+    "revision": "4b059261683b5be75625",
+    "url": "/portfolio2/static/js/runtime-main.1ee5a9c2.js"
+  },
+  {
+    "revision": "462bd259d98d98460d6767edbdcdac70",
+    "url": "/portfolio2/static/media/FoobarPro-Regular.462bd259.otf"
+  },
+  {
+    "revision": "99ee5c500489d6812c5ab1c981ce99bf",
+    "url": "/portfolio2/static/media/designer.99ee5c50.png"
+  },
+  {
+    "revision": "80a1b4a6a54d5a3999374189687d1e82",
+    "url": "/portfolio2/static/media/dribbble.80a1b4a6.jpg"
+  },
+  {
+    "revision": "ec945afa1a5a55a91815b4cf8595038a",
+    "url": "/portfolio2/static/media/koalacare.ec945afa.png"
+  },
+  {
+    "revision": "b15d81546dd2d6fad2a2b3aeb7f245e8",
+    "url": "/portfolio2/static/media/lead-of-engineering.b15d8154.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio2/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9bb533b247866d1652925aba4234dcc0",
+    "url": "/portfolio2/static/media/technical-architect.9bb533b2.png"
+  },
+  {
+    "revision": "62dff354ae0be8de5a2eed05408d0708",
+    "url": "/portfolio2/static/media/webDev.62dff354.png"
+  }
+]);
